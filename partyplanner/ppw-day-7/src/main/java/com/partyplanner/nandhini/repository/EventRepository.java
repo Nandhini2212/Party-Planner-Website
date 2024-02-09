7f7f02fb28bac6db462488f765a93e8bdb101369
Nandhini2212 <113581603+Nandhini2212@users.noreply.github.com>
2024-02-09 15:40:41 +0530

    Delete partyplanner/ppw-day-7/src/main/java/com/partyplanner/nandhini/repository directory

--- partyplanner/ppw-day-7/src/main/java/com/partyplanner/nandhini/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.partyplanner.nandhini.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.partyplanner.nandhini.model.Event;
-
-public interface EventRepository extends JpaRepository<Event,Long> {
-
-}
