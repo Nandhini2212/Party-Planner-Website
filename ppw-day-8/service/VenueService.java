4beef9485e1f01a96bcbbd836e2095ad8a20dccc
Nandhini2212 <113581603+Nandhini2212@users.noreply.github.com>
2024-02-09 10:00:06 +0530

    Add files via upload

---
+++ ppw-day-8/service/VenueService.java
@@ -0,0 +1,17 @@
+package com.partyplanner.nandhini.service;
+
+import java.util.List;
+
+import com.partyplanner.nandhini.dto.request.VenueRequest;
+import com.partyplanner.nandhini.dto.response.VenueResponse;
+import com.partyplanner.nandhini.model.Venue;
+
+public interface VenueService {
+    VenueResponse createVenue(VenueRequest request);
+
+    List<Venue> getAllVenues();
+
+
+    
+
+}
